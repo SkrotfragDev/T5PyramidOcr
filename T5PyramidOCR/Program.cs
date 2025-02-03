ca93465fc3e6e711be62d088a2214ae3c3a5aee2
SkrotfragDev <markus.hansson@skrotfrag.se>
2025-02-03 13:53:09 +0100

    v2

--- T5PyramidOCR/Program.cs
+++ T5PyramidOCR/Program.cs
@@ -9,14 +9,20 @@ class Program
     {
         if (args.Length < 2)
         {
-            Console.WriteLine("Usage: T5PyramidOCR <sourceDirectory> <destinationDirectory>");
+            Console.WriteLine("Usage: T5PyramidOCR <sourceDirectory> <destinationDirectory> [rePlaceFa]=0");
             return;
         }
 
         string sourceDirectory = args[0];
         string destinationDirectory = args[1];
         //string searchCodeOCR = args[2];
-        string searchCodeOCR = "#11349;"; 
+        string searchCodeOCR = "#11349;";
+        string searchCodeFA = "#11304;";
+        bool replaceFA = false;
+        if (args.Length > 2)
+        {
+            replaceFA = args[2] == "1" ? true :false ;
+        }
 
         Directory.CreateDirectory(destinationDirectory);
 
@@ -29,7 +35,7 @@ class Program
                 if (lines[i].Contains(searchCodeOCR))
                 {
                     int index = lines[i].IndexOf(searchCodeOCR) + searchCodeOCR.Length;
-                    string faNr = lines[i].Substring(index).Replace("-","");
+                    string faNr = lines[i].Substring(index).Replace("-", "");
                     string len = (faNr.Length + 2).ToString();
                     if (len.Length > 1)
                         len = len.Substring(1);
@@ -37,10 +43,24 @@ class Program
                     string ocr = faNr + len + kk;
                     lines[i] = lines[i].Substring(0, index) + ocr; // replacementCode;
                 }
+                if (lines[i].Contains(searchCodeFA))
+                {
+                    if (replaceFA)
+                    {
+                        int index = lines[i].IndexOf(searchCodeFA) + searchCodeFA.Length;
+                        string faNr = lines[i].Substring(index).Replace("-", "");
+                        string len = (faNr.Length + 2).ToString();
+                        if (len.Length > 1)
+                            len = len.Substring(1);
+                        var kk = LuhnDotNet.Luhn.ComputeLuhnCheckDigit(faNr + len);
+                        string fa = faNr + len + kk;
+                        lines[i] = lines[i].Substring(0, index) + fa; // replacementCode;
+                    }
+                }
             }
             string newContent = string.Join(Environment.NewLine, lines);
             string newFilePath = Path.Combine(destinationDirectory, Path.GetFileName(filePath));
-            File.WriteAllText(newFilePath, newContent, Encoding.GetEncoding(28591) );
+            File.WriteAllText(newFilePath, newContent, Encoding.GetEncoding(28591));
         });
     }
 }
